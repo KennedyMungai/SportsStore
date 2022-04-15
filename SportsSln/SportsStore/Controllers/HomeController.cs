9f697a5a1e8f59bfab65b5fa6dfa3ee512010014
KennedyMungai <kennedymungaifmab@gmail.com>
2022-04-15 14:57:51 +0300

    Added pagination code. Something is buggy

--- SportsSln/SportsStore/Controllers/HomeController.cs
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers;
 
@@ -7,10 +8,26 @@ public class HomeController : Controller
 {
     private IStoreRepository repository;
 
+    public int PageSize = 4;
+
     public HomeController(IStoreRepository repo)
     {
         repository = repo;
     }
 
-    public IActionResult Index() => View(repository.Products);
+    public IActionResult Index(int productPage = 1) 
+        => View(
+            new ProductsListViewModel
+            {
+                Products = repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize),PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            }
+        );
 }
