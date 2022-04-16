4b0e728dfa91d9864db292d7793e99efe29e3114
KennedyMungai <kennedymungaifmab@gmail.com>
2022-04-16 18:14:52 +0300

    Have no idea what happened but my head aches

--- SportsSln/SportsStore/Pages/Cart.cshtml.cs
+++ SportsSln/SportsStore/Pages/Cart.cshtml.cs
@@ -9,9 +9,10 @@ namespace SportsStore.Pages
     {
         private IStoreRepository repository;
 
-        public CartModel(IStoreRepository repo)
+        public CartModel(IStoreRepository repo, Cart cartService)
         {
             repository = repo;
+            Cart = cartService;
         }
 
         public Cart? Cart { get; set; }
@@ -21,7 +22,7 @@ namespace SportsStore.Pages
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            // Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         }
 
         [HttpPost]
@@ -32,12 +33,19 @@ namespace SportsStore.Pages
 
             if (product != null)
             {
-                Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+                //Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
                 Cart.AddItem(product, 1);
-                HttpContext.Session.SetJson("cart", Cart);
+                //HttpContext.Session.SetJson("cart", Cart);
             }
 
             return RedirectToPage(new {returnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        public IActionResult OnPostRemove(long productId, string returnUrl)
+        {
+            Cart.RemoveLine(Cart.Lines.First(cl => cl.Product.ProductID == productId).Product);
+            return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
 }
