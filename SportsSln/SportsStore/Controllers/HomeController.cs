1887b3061bb036d8ef4fa476a4b957060d2947c4
KennedyMungai <kennedymungaifmab@gmail.com>
2022-04-16 16:28:42 +0300

    Commiting to add a shopping cart

--- SportsSln/SportsStore/Controllers/HomeController.cs
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -15,19 +15,24 @@ public class HomeController : Controller
         repository = repo;
     }
 
-    public IActionResult Index(int productPage = 1) 
-        => View(
-            new ProductsListViewModel
-            {
-                Products = repository.Products
-                .OrderBy(p => p.ProductID)
-                .Skip((productPage - 1) * PageSize)
-                .Take(PageSize),PagingInfo = new PagingInfo
-                {
-                    CurrentPage = productPage,
-                    ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
+    public IActionResult Index(string? category, int productPage = 1) 
+        => View(new ProductsListViewModel
+                { 
+                    Products = repository.Products
+                        .Where(p => category == null || p.Category == category)
+                        .OrderBy(p => p.ProductID)
+                        .Skip((productPage - 1) * PageSize)
+                        .Take(PageSize),
+                    PagingInfo = new PagingInfo
+                    {
+                        CurrentPage = productPage,
+                        ItemsPerPage = PageSize,
+                        TotalItems = category == null
+                            ? repository.Products.Count()
+                            : repository.Products.Where(e => e.Category == category).Count()
+                    },
+
+                    CurrentCategory = category
                 }
-            }
-        );
+            );
 }
