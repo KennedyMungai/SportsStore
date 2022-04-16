903f457f764f589b2c26cfdbadcf3d09201079c8
KennedyMungai <kennedymungaifmab@gmail.com>
2022-04-16 17:25:13 +0300

    Finished the cart functionality for now

--- SportsSln/SportsStore/Program.cs
+++ SportsSln/SportsStore/Program.cs
@@ -9,11 +9,15 @@ builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 
 builder.Services.AddRazorPages();
 
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+
 var app = builder.Build();
 
 // app.MapGet("/", () => "Hello World!");
 
 app.UseStaticFiles();
+app.UseSession();
 
 app.MapControllerRoute("catpage","{category}/Page{productPage:int}",new {Controller = "Home", action = "Index"});
 app.MapControllerRoute("page","Page{productPage:int}", new {Controller = "Home", action = "Index", productPage = 1});
@@ -21,6 +25,8 @@ app.MapControllerRoute("category", "{category}", new {Controller = "Home", actio
 app.MapControllerRoute("pagination", "Products/Page{productPage}", new {Controller = "Home", action = "Index", productPage = 1});
 
 app.MapDefaultControllerRoute();
+
+app.MapRazorPages();
 
 SeedData.EnsurePopulated(app);
 
