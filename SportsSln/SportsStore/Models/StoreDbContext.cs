89b6845e67e51626db3dcbd1250d9a6a66572e3c
KennedyMungai <kennedymungaifmab@gmail.com>
2022-04-17 10:10:34 +0300

    Added order placing functionality to the web app

--- SportsSln/SportsStore/Models/StoreDbContext.cs
+++ SportsSln/SportsStore/Models/StoreDbContext.cs
@@ -5,8 +5,8 @@ namespace SportsStore.Models;
 public class StoreDbContext : DbContext
 {
     public DbSet<Product> Products => Set<Product>();
-    
-    
+
+    public DbSet<Order> Orders => Set<Order>();
 
     public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
     {
--- SportsSln/SportsStore/Program.cs
+++ SportsSln/SportsStore/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<StoreDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SportsStoreConnection")));
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+builder.Services.AddScoped<IOrderRepository, EFOrderRepository>();
 
 builder.Services.AddRazorPages();
 
