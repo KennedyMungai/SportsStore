e3f979510148a340499b5cbac5b06d61be1da110
KennedyMungai <kennedymungaifmab@gmail.com>
2022-04-18 12:54:13 +0300

    Don't understand but still learning

--- SportsSln/Platform/Program.cs
+++ SportsSln/Platform/Program.cs
@@ -1,15 +1,61 @@
+using Microsoft.Extensions.Options;
+using Platform;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MessageOptions>(options =>
+    {
+        options.CityName = "Albany";
+    }
+);
+
 var app = builder.Build();
 
-app.Use(async (context, next) =>
+((IApplicationBuilder)app).Map("/branch", async branch =>
     {
-        if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+        branch.UseMiddleware<Platform.QueryStringMiddleWare>();
+
+        branch.Use(async (HttpContext context, Func<Task> next) =>
+            {
+                await context.Response.WriteAsync($"Branch Middleware");
+            }
+        );
+    }
+);
+
+// app.Use(async (context, next) =>
+//     {
+//         if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+//         {
+//             context.Response.ContentType = "text/plain";
+//             await context.Response.WriteAsync("Custom Middleware \n");
+//         }
+//     }
+// );
+
+app.Use(async (context, next) => 
+    {
+        await next();
+        await context.Response
+            .WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
+    }
+);
+
+app.Use(async (context, next) => 
+    {
+        if (context.Request.Path == "/short")
         {
-            context.Response.ContentType = "text/plain";
-            await context.Response.WriteAsync("Custom Middleware \n");
+            await context.Response
+                .WriteAsync($"Request short circuited");
+        }
+        else
+        {
+            await next();
         }
     }
 );
+
+app.UseMiddleware<Platform.QueryStringMiddleWare>();
 
 app.MapGet("/", () => "Hello World!");
 
